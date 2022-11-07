1ec709e39887c1c061ab7a44f1fd7cc0db46423c
Sergio De La Cruz <seldca@gmail.com>
2022-11-07 11:36:32 -0500

    Prueba Sergio1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ui/my_app.dart';
 
-// punto de entrada de la aplicación
+// punto de entrada de la aplicación, probando cambios
 void main() {
   runApp(const MyApp());
 }
