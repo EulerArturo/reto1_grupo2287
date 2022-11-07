418bb4e09855fcbff97b02dc24748323d2cedd7e
euler_chapid <arturo.chapid1992@hotmail.com>
2022-11-07 11:46:51 -0500

    prueva con sergio 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ui/my_app.dart';
 
-// punto de entrada de la aplicación, probando cambios
+// punto de entrada de la aplicación
 void main() {
   runApp(const MyApp());
 }
